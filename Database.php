4780d7ba16f238b935eab87c5872ff55963f8406
ivvns <ivansomera.hotmail.com>
2024-09-17 13:12:43 -0700

    Binded named params, created routes, controller, and view  for single listing

--- Database.php
+++ Database.php
@@ -34,9 +34,15 @@ class Database {
      * @throws PDOException
      */
 
-     public function query($query) {
+     public function query($query, $params = []) {
         try {
             $sth  = $this->conn->prepare($query);
+
+            // Bind named params
+            foreach($params as $param => $value) {
+                $sth->bindValue(':' . $param, $value);
+            }
+
             $sth->execute();
             return $sth;
 
--- views/home.view.php
+++ views/home.view.php
@@ -5,8 +5,6 @@
 
     <?= loadPartial('top-banner') ?>
 
-
-
     <!-- Job Listings -->
     <section>
       <div class="container mx-auto p-4 mt-4">
@@ -31,7 +29,7 @@
                   <strong>Tags:</strong> <?= $listing->tags ?>
                 </li>
               </ul>
-              <a href="/listing/<?= $listing->id ?>"
+              <a href="/listing?id=<?= $listing->id ?>"
                 class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
               >
                 Details
