bef9411de8122e52989050219ee408c7f3c04b3c
ivvns <ivansomera.hotmail.com>
2024-09-17 23:18:16 -0700

    Creating Session class and setting users

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Framework\Database;
 use Framework\Validation;
+use Framework\Session;
 
 class UserController {
     protected $db;
@@ -102,6 +103,18 @@ class UserController {
         ];
 
         $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+
+        // Get new user ID 
+        $userId = $this->db->conn->lastInsertId();
+
+        Session::set('user', [
+            'id' => $userId, 
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state
+
+        ]);
 
         redirect('/');
     }
